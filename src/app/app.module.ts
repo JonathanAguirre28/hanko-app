ed523add4f8a5be7dfff25c4fa2515e4493c0bd8
JonathanAguirre <jonathan621924@gmail.com>
2023-09-14 15:06:25 -0300

    generando el componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
