b5e1a572cdb58a1406fc83fa88a0548c95eab818
JonathanAguirre <jonathan621924@gmail.com>
2023-09-18 18:22:49 -0300

    enrutando los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: '/auth/login',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
