c4b8524f71e1374cc23a1272885994756c1f8b60
RamosGustavo <gr4030077@gmail.com>
2023-09-28 09:59:38 -0300

    menu

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,6 +5,11 @@ import { NavMenuComponent } from './loyout/nav-menu/nav-menu.component';
 import { ToolbarComponent } from './loyout/toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -18,6 +23,11 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     RouterOutlet,
     SharedModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class DashboardModule { }
