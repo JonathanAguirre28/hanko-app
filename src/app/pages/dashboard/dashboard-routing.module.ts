453c798c1a8f708f11c1e84eeb01352ec7a1f490
Felipeunicomer <felipe_ramos@outsource.unicomer.com>
2023-10-14 13:45:43 -0300

    routing fix resolve

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -4,37 +4,34 @@ import { UsersComponent } from './pages/users/users.component';
 import { ClientsComponent } from './pages/clients/clients.component';
 import { DrinksComponent } from './pages/drinks/drinks.component';
 import { FinanceComponent } from './pages/finance/finance.component';
-import { DashboardComponent } from './dashboard.component';
 import { ExercisesComponent } from './pages/exercises/exercises.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: '',
-        component: DashboardComponent,
-        children: [
-          {
-            path: 'usuarios',
-            component: UsersComponent,
-          },
-          {
-            path: 'clientes',
-            component: ClientsComponent,
-          },
-          {
-            path: 'ejercicios',
-            component: ExercisesComponent,
-          },
-          {
-            path: 'bebidas',
-            component: DrinksComponent,
-          },
-          {
-            path: 'finanzas',
-            component: FinanceComponent,
-          },
-        ],
+        path: 'usuarios',
+        component: UsersComponent,
+      },
+      {
+        path: 'clientes',
+        component: ClientsComponent,
+      },
+      {
+        path: 'ejercicios',
+        component: ExercisesComponent,
+      },
+      {
+        path: 'bebidas',
+        component: DrinksComponent,
+      },
+      {
+        path: 'finanzas',
+        component: FinanceComponent,
+      },
+      {
+        path: '**',
+        component: UsersComponent,
       },
     ]),
   ],
